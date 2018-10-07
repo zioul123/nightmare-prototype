979a7fe5f09d9b0e1ceb07a96ec2a8c6340d8d3f
zioul123 <zioul.rsmod123@gmail.com>
2018-10-07 20:33:18 +0800

    Add a sandbox spawner for testing.

---
+++ Assets/Scripts/Sandbox/RandomSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour 
+{
+    // Activate/Deactivate the spawner
+    [SerializeField]
+    private bool active;
+    [SerializeField]
+    private GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (active) {
+            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
+            if (gameObjects.Length < 10)
+            {
+                float x = Random.Range(-9f, 9f);
+                float y = Random.Range(-4.5f, 4.5f);
+
+                Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);
+
+            }
+        }
+	}
+}
