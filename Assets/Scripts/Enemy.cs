fda113ccf738cfa070ed936fb86da84e5f0c7a92
zioul123 <zioul.rsmod123@gmail.com>
2018-10-16 10:18:47 +0800

    Health bars appear on damaged.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,8 +17,8 @@ public class Enemy : Npc
     public override Transform Select()
     {
         // Show health group and selection circle
-        healthGroup.alpha = 1;
-        selectionCircle.gameObject.SetActive(true);
+        ShowHealthGroup();
+        ShowSelectionCircle();
 
         // Stop the hiding health coroutine from completing if it exists
         if (hideHealthRoutine != null) {
@@ -35,26 +35,74 @@ public class Enemy : Npc
         base.Deselect();
         // If the enemy is undamaged, hide health
         if (Health.CurrentValue == Health.MaxValue) {
-            healthGroup.alpha = 0;
+            HideHealthGroup();
         // Otherwise make it disappear after a few seconds
         } else {
-            hideHealthRoutine = StartCoroutine(HideHealthGroup());
+            hideHealthRoutine = StartCoroutine(HideHealthGroupAfterDelay());
         }
-        selectionCircle.gameObject.SetActive(false);
+        HideSelectionCircle();
     }
 
     // Hide the healthgroup after a few seconds delay
-    public IEnumerator HideHealthGroup() {
+    public IEnumerator HideHealthGroupAfterDelay() {
         yield return new WaitForSeconds(5);
-        healthGroup.alpha = 0;
+        HideHealthGroup();
         hideHealthRoutine = null;
     }
 
-    // Perform character functions and call NPC's function that activates listeners
+    // Perform character functions and call NPC's function that activates listeners. Also show health group for a short while.
     public override void TakeDamage(float damage)
     {
         base.TakeDamage(damage);
         OnHealthChange(Health.CurrentValue);
+
+        // Health is not currently shown - not selected nor attacked within 5 seconds ago
+        if (healthGroup.alpha == 0) {
+            ShowHealthGroup();
+            // Restart the hide health timer if necessary
+            if (hideHealthRoutine != null)
+            {
+                StopCoroutine(hideHealthRoutine);
+                hideHealthRoutine = null;
+            }
+            hideHealthRoutine = StartCoroutine(HideHealthGroupAfterDelay());
+        // Health is already shown
+        } else {
+            // It is there by selection
+            if (hideHealthRoutine == null) {
+                return;
+            // It is there by damage
+            } else if (hideHealthRoutine != null) {
+                // Restart the hide health timer if necessary
+                StopCoroutine(hideHealthRoutine);
+                hideHealthRoutine = null;
+                hideHealthRoutine = StartCoroutine(HideHealthGroupAfterDelay());
+            }
+        }
+    }
+
+    // Show the health group
+    private void ShowHealthGroup() 
+    {
+        healthGroup.alpha = 1;
+    }
+
+    // Show the selection circle
+    private void ShowSelectionCircle() 
+    {
+        selectionCircle.gameObject.SetActive(true);
+    }
+
+    // Hide the health group
+    private void HideHealthGroup()
+    {
+        healthGroup.alpha = 0;
+    }
+
+    // Hide  the selection circle
+    private void HideSelectionCircle()
+    {
+        selectionCircle.gameObject.SetActive(false);
     }
 
 }
